cae656799717d6f1e8c5da33ed63798375d22f13
McKenzie <mckenzielynn@ibm.com>
2023-01-31 13:07:53 -0600

    Adding Upload component

---
+++ src/Components/MainPage/Upload/Upload.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Upload.css';
+
+class Upload extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() { 
+        return ( 
+            <div>
+                Upload Panel 
+            </div>
+         );
+    }
+}
+ 
+export default Upload;
