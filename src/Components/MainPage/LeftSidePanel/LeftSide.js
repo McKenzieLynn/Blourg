9394bd6c549823707306ed3010fe182eb7d428d7
McKenzie <mckenzielynn@ibm.com>
2023-01-31 11:21:21 -0600

    adding more to left side

--- src/Components/MainPage/LeftSidePanel/LeftSide.js
+++ src/Components/MainPage/LeftSidePanel/LeftSide.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import "./LeftSide.css";
 import ImageLayout from '../ImageLayout';
+import pfp from "../../../Images/PFPs/pfp.png"
 import HeartHand from "../../../Images/Icons/HeartHand.png";
 import Friends from "../../../Images/Icons/Friends.png";
 import Picture from "../../../Images/Icons/Picture.png";
@@ -8,6 +9,9 @@ import Clock from "../LeftSidePanel/Clock/Clock.js";
 import Calendar from "../../../Images/Icons/Calendar.png";
 import Community from "../../../Images/Icons/Community.png";
 import Library from "../../../Images/Icons/Library.png";
+import Graph from "../../../Images/Icons/Graph.png";
+import Shopping from "../../../Images/Icons/Shopping.png";
+import Settings from "../../../Images/Icons/Settings.png";
 
 class LeftSide extends Component {
   constructor(props) {
@@ -19,6 +23,10 @@ class LeftSide extends Component {
 
   getData=()=>{
     let jsondata = [
+        {
+          "image": pfp,
+          "text":"Kenzie Lynn Hearnsberger",
+        },
         {
           "image": HeartHand,
           "text":"Mental Health Advocacy",
@@ -42,6 +50,18 @@ class LeftSide extends Component {
         {
           "image":Community,
           "text":"Community Hub",
+        },
+        {
+          "image":Graph,
+          "text":"Viewer Tracking",
+        },
+        {
+          "image":Shopping,
+          "text":"Stationary Supplies",
+        },
+        {
+          "image":Settings,
+          "text":"Settings",
         },
     ];
     this.setState({data : jsondata});
