f3afb20c9896ebad1aeb9e0a4de3b885599b5c9d
McKenzie <mckenzielynn@ibm.com>
2023-02-06 12:45:38 -0600

    Finishing upload section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Layout from './Components/MainPage/Layout';
 import NavBar from './Components/NavBar/NavBar';
-
-
+const bgimage = new URL('./Images/Background.jpg', import.meta.url)
 
 function App() {
   return (
     <div className="App">
+        <img className="background" src={ bgimage } alt="bg" />
         <NavBar />
         <Layout />
     </div>
--- src/Components/MainPage/Upload/Upload.js
+++ src/Components/MainPage/Upload/Upload.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import './Upload.css';
 import { Paper, Avatar } from '@mui/material';
 import pfp from "../../../Images/PFPs/pfp.png";
+import Pencil from "../../../Images/Icons/Pencil.png";
+import PhotoAdd from "../../../Images/Icons/PhotoAdd.png";
+import Mood from "../../../Images/Icons/Mood.png";
 
 class Upload extends Component {
     constructor(props) {
@@ -21,7 +24,18 @@ class Upload extends Component {
                     </div>
                 </div>
                 <div className="upload__bottom">
-
+                    <div classname="upload__tabs">
+                        <img src={Pencil} width="35px" alt="Writing" />
+                        {/* <div className="upload__text">Writing</div> */}
+                    </div>
+                    <div classname="upload__tabs">
+                        <img src={PhotoAdd} width="35px" alt="Photoadd"/>
+                        {/* <div className="upload__text">Photo</div> */}
+                    </div>
+                    <div classname="upload__tabs">
+                        <img src={Mood} width="35px" alt="Mood"/>
+                        {/* <div className="upload__text">Mood</div> */}
+                    </div>
                 </div>
             </Paper>
         </div>
